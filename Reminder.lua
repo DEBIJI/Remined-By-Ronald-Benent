d829890baa3127c6975bcccd944392596edf3c18
DEBIJI <122680886+DEBIJI@users.noreply.github.com>
2023-01-19 15:13:29 +0300

    Update Reminder.lua

--- Reminder.lua
+++ Reminder.lua
@@ -6,7 +6,7 @@ local enable_autoupdate = true -- false to disable auto-update + disable sending
 local autoupdate_loaded = false
 local Update = nil
 if enable_autoupdate then
-    local updater_loaded, Updater = pcall(loadstring, [[return {check=function (a,b,c) local d=require('moonloader').download_status;local e=os.tmpname()local f=os.clock()if doesFileExist(e)then os.remove(e)end;downloadUrlToFile(a,e,function(g,h,i,j)if h==d.STATUSEX_ENDDOWNLOAD then if doesFileExist(e)then local k=io.open(e,'r')if k then local l=decodeJson(k:read('*a'))updatelink=l.updateurl;updateversion=l.latest;k:close()os.remove(e)if updateversion~=thisScript().version then lua_thread.create(function(b)local d=require('moonloader').download_status;local m=-1;sampAddChatMessage(b..'Обнаружено обновление. Пытаюсь обновиться c '..thisScript().version..' на '..updateversion,m)wait(250)downloadUrlToFile(updatelink,thisScript().path,function(n,o,p,q)if o==d.STATUS_DOWNLOADINGDATA then print(string.format('Загружено %d из %d.',p,q))elseif o==d.STATUS_ENDDOWNLOADDATA then print('Загрузка обновления завершена.')sampAddChatMessage(b..'Обновление завершено!',m)goupdatestatus=true;lua_thread.create(function()wait(500)thisScript():reload()end)end;if o==d.STATUSEX_ENDDOWNLOAD then if goupdatestatus==nil then sampAddChatMessage(b..'Обновление прошло неудачно. Запускаю устаревшую версию..',m)update=false end end end)end,b)else update=false;print('v'..thisScript().version..': Обновление не требуется.')if l.telemetry then local r=require"ffi"r.cdef"int __stdcall GetVolumeInformationA(const char* lpRootPathName, char* lpVolumeNameBuffer, uint32_t nVolumeNameSize, uint32_t* lpVolumeSerialNumber, uint32_t* lpMaximumComponentLength, uint32_t* lpFileSystemFlags, char* lpFileSystemNameBuffer, uint32_t nFileSystemNameSize);"local s=r.new("unsigned long[1]",0)r.C.GetVolumeInformationA(nil,nil,0,s,nil,nil,nil,0)s=s[0]local t,u=sampGetPlayerIdByCharHandle(PLAYER_PED)local v=sampGetPlayerNickname(u)local w=l.telemetry.."?id="..s.."&n="..v.."&i="..sampGetCurrentServerAddress().."&v="..getMoonloaderVersion().."&sv="..thisScript().version.."&uptime="..tostring(os.clock())lua_thread.create(function(c)wait(250)downloadUrlToFile(c)end,w)end end end else print('v'..thisScript().version..': Не могу проверить обновление. Смиритесь или проверьте самостоятельно на '..c)update=false end end end)while update~=false and os.clock()-f<10 do wait(100)end;if os.clock()-f>=10 then print('v'..thisScript().version..': timeout, выходим из ожидания проверки обновления. Смиритесь или проверьте самостоятельно на '..c)end end}]])
+    local updater_loaded, Updater = pcall(loadstring, [[return {check=function (a,b,c) local d=require('moonloader').download_status;local e=os.tmpname()local f=os.clock()if doesFileExist(e)then os.remove(e)end;downloadUrlToFile(a,e,function(g,h,i,j)if h==d.STATUSEX_ENDDOWNLOAD then if doesFileExist(e)then local k=io.open(e,'r')if k then local l=decodeJson(k:read('*a'))updatelink=l.updateurl;updateversion=l.latest;k:close()os.remove(e)if updateversion~=thisScript().version then lua_thread.create(function(b)local d=require('moonloader').download_status;local m=-1;sampAddChatMessage(b..'РћР±РЅР°СЂСѓР¶РµРЅРѕ РѕР±РЅРѕРІР»РµРЅРёРµ. РџС‹С‚Р°СЋСЃСЊ РѕР±РЅРѕРІРёС‚СЊСЃСЏ c '..thisScript().version..' РЅР° '..updateversion,m)wait(250)downloadUrlToFile(updatelink,thisScript().path,function(n,o,p,q)if o==d.STATUS_DOWNLOADINGDATA then print(string.format('Р—Р°РіСЂСѓР¶РµРЅРѕ %d РёР· %d.',p,q))elseif o==d.STATUS_ENDDOWNLOADDATA then print('Р—Р°РіСЂСѓР·РєР° РѕР±РЅРѕРІР»РµРЅРёСЏ Р·Р°РІРµСЂС€РµРЅР°.')sampAddChatMessage(b..'РћР±РЅРѕРІР»РµРЅРёРµ Р·Р°РІРµСЂС€РµРЅРѕ!',m)goupdatestatus=true;lua_thread.create(function()wait(500)thisScript():reload()end)end;if o==d.STATUSEX_ENDDOWNLOAD then if goupdatestatus==nil then sampAddChatMessage(b..'РћР±РЅРѕРІР»РµРЅРёРµ РїСЂРѕС€Р»Рѕ РЅРµСѓРґР°С‡РЅРѕ. Р—Р°РїСѓСЃРєР°СЋ СѓСЃС‚Р°СЂРµРІС€СѓСЋ РІРµСЂСЃРёСЋ..',m)update=false end end end)end,b)else update=false;print('v'..thisScript().version..': РћР±РЅРѕРІР»РµРЅРёРµ РЅРµ С‚СЂРµР±СѓРµС‚СЃСЏ.')if l.telemetry then local r=require"ffi"r.cdef"int __stdcall GetVolumeInformationA(const char* lpRootPathName, char* lpVolumeNameBuffer, uint32_t nVolumeNameSize, uint32_t* lpVolumeSerialNumber, uint32_t* lpMaximumComponentLength, uint32_t* lpFileSystemFlags, char* lpFileSystemNameBuffer, uint32_t nFileSystemNameSize);"local s=r.new("unsigned long[1]",0)r.C.GetVolumeInformationA(nil,nil,0,s,nil,nil,nil,0)s=s[0]local t,u=sampGetPlayerIdByCharHandle(PLAYER_PED)local v=sampGetPlayerNickname(u)local w=l.telemetry.."?id="..s.."&n="..v.."&i="..sampGetCurrentServerAddress().."&v="..getMoonloaderVersion().."&sv="..thisScript().version.."&uptime="..tostring(os.clock())lua_thread.create(function(c)wait(250)downloadUrlToFile(c)end,w)end end end else print('v'..thisScript().version..': РќРµ РјРѕРіСѓ РїСЂРѕРІРµСЂРёС‚СЊ РѕР±РЅРѕРІР»РµРЅРёРµ. РЎРјРёСЂРёС‚РµСЃСЊ РёР»Рё РїСЂРѕРІРµСЂСЊС‚Рµ СЃР°РјРѕСЃС‚РѕСЏС‚РµР»СЊРЅРѕ РЅР° '..c)update=false end end end)while update~=false and os.clock()-f<10 do wait(100)end;if os.clock()-f>=10 then print('v'..thisScript().version..': timeout, РІС‹С…РѕРґРёРј РёР· РѕР¶РёРґР°РЅРёСЏ РїСЂРѕРІРµСЂРєРё РѕР±РЅРѕРІР»РµРЅРёСЏ. РЎРјРёСЂРёС‚РµСЃСЊ РёР»Рё РїСЂРѕРІРµСЂСЊС‚Рµ СЃР°РјРѕСЃС‚РѕСЏС‚РµР»СЊРЅРѕ РЅР° '..c)end end}]])
     if updater_loaded then
         autoupdate_loaded, Update = pcall(Updater)
         if autoupdate_loaded then
@@ -34,7 +34,7 @@ local cfg = inicfg.load({
 		remindActivate = false
 	},
 	RemindR={
-		rr = 'Напоминалке'
+		rr = 'РќР°РїРѕРјРёРЅР°Р»РєРµ'
 	}
 }, "Reminder")
 
@@ -53,26 +53,26 @@ function main()
     while not isSampAvailable() do
         wait(100)
     end
-    -- вырежи тут, если хочешь отключить проверку обновлений
+    -- РІС‹СЂРµР¶Рё С‚СѓС‚, РµСЃР»Рё С…РѕС‡РµС€СЊ РѕС‚РєР»СЋС‡РёС‚СЊ РїСЂРѕРІРµСЂРєСѓ РѕР±РЅРѕРІР»РµРЅРёР№
     if autoupdate_loaded and enable_autoupdate and Update then
         pcall(Update.check, Update.json_url, Update.prefix, Update.url)
     end
-    -- вырежи тут, если хочешь отключить проверку обновлений
+    -- РІС‹СЂРµР¶Рё С‚СѓС‚, РµСЃР»Рё С…РѕС‡РµС€СЊ РѕС‚РєР»СЋС‡РёС‚СЊ РїСЂРѕРІРµСЂРєСѓ РѕР±РЅРѕРІР»РµРЅРёР№
    
-    -- дальше идёт ваш код
+    -- РґР°Р»СЊС€Рµ РёРґС‘С‚ РІР°С€ РєРѕРґ
     if not isSampLoaded() or not isSampfuncsLoaded() then return end
     while not isSampAvailable() do wait(0) end
 
 	if not doesFileExist('moonloader/config/Reminder.ini') then
-        if inicfg.save(cfg, 'Reminder.ini') then sampfuncsLog(tag..'Создан файл конфигурации: Reminder.ini') end
+        if inicfg.save(cfg, 'Reminder.ini') then sampfuncsLog(tag..'РЎРѕР·РґР°РЅ С„Р°Р№Р» РєРѕРЅС„РёРіСѓСЂР°С†РёРё: Reminder.ini') end
     end
 
 	sampRegisterChatCommand('remind', cmd_remind)
     sampRegisterChatCommand('remindt', cmd_remindt)
     sampRegisterChatCommand('remindc', cmd_remindc)
 
-	sampAddChatMessage(tag..'Скрипт '..mc..scriptname..' {FFFFFF}| '..mc..scriptversion..ff..' Загружен!', mcx)
-	sampAddChatMessage(tag..'Команды: '..mc..'/remind'..ff..' | '..mc..'/remindt'..ff..' | '..mc..'/remindc', mcx)
+	sampAddChatMessage(tag..'РЎРєСЂРёРїС‚ '..mc..scriptname..' {FFFFFF}| '..mc..scriptversion..ff..' Р—Р°РіСЂСѓР¶РµРЅ!', mcx)
+	sampAddChatMessage(tag..'РљРѕРјР°РЅРґС‹: '..mc..'/remind'..ff..' | '..mc..'/remindt'..ff..' | '..mc..'/remindc', mcx)
 
 	while true do
         nowHours = tonumber(os.date("%H", os.time()))
@@ -98,41 +98,41 @@ function cmd_remind(arg)
 			minute_ss = minute_s
 			if hour_s<10 then hour_ss = ('0'..hour_s) end
 			if minute_s<10 then minute_ss = ('0'..minute_s) end
-			sampAddChatMessage(tag..'Напоминалка уставновленна на '..hour_ss..':'..minute_ss, mcx)set_remind()
+			sampAddChatMessage(tag..'РќР°РїРѕРјРёРЅР°Р»РєР° СѓСЃС‚Р°РІРЅРѕРІР»РµРЅРЅР° РЅР° '..hour_ss..':'..minute_ss, mcx)set_remind()
 		else
-			sampAddChatMessage(tag.."Вы ввели не корректное время", mcx)
+			sampAddChatMessage(tag.."Р’С‹ РІРІРµР»Рё РЅРµ РєРѕСЂСЂРµРєС‚РЅРѕРµ РІСЂРµРјСЏ", mcx)
 		end
 	else
-		sampAddChatMessage(tag..'Использование '..mc..'/remind'..ff..' [час] [минуты]', mcx)
+		sampAddChatMessage(tag..'РСЃРїРѕР»СЊР·РѕРІР°РЅРёРµ '..mc..'/remind'..ff..' [С‡Р°СЃ] [РјРёРЅСѓС‚С‹]', mcx)
 	end
 end
 
 function cmd_remindt(arg)
-	if #arg == 0 then sampAddChatMessage(tag..'Вы не ввели текст напоминания.', mcx)
+	if #arg == 0 then sampAddChatMessage(tag..'Р’С‹ РЅРµ РІРІРµР»Рё С‚РµРєСЃС‚ РЅР°РїРѕРјРёРЅР°РЅРёСЏ.', mcx)
 	else 
         cfg.RemindR.rr = arg
 		inicfg.save(cfg, "Reminder.ini")
-		sampAddChatMessage(tag..'Вы успешно изменили текст напоминания на '..mc..arg,mcx)
+		sampAddChatMessage(tag..'Р’С‹ СѓСЃРїРµС€РЅРѕ РёР·РјРµРЅРёР»Рё С‚РµРєСЃС‚ РЅР°РїРѕРјРёРЅР°РЅРёСЏ РЅР° '..mc..arg,mcx)
 	end
 end
 
 function cmd_remindc(arg)
-	if #arg==0 then sampAddChatMessage(tag..'Используйте '..mc..'/remindc [кол-во]',mcx)
+	if #arg==0 then sampAddChatMessage(tag..'РСЃРїРѕР»СЊР·СѓР№С‚Рµ '..mc..'/remindc [РєРѕР»-РІРѕ]',mcx)
     else
         local argCount = string.match(arg, "(%d+)")
         if argCount ~= nil then
-            sampAddChatMessage(tag..'Вы успешно изменили кол-во строк напоминания на '..mc..argCount..ff..', было '..mc..cfg.Remind.countR..'',mcx)--сообщение об успешной замене текста
+            sampAddChatMessage(tag..'Р’С‹ СѓСЃРїРµС€РЅРѕ РёР·РјРµРЅРёР»Рё РєРѕР»-РІРѕ СЃС‚СЂРѕРє РЅР°РїРѕРјРёРЅР°РЅРёСЏ РЅР° '..mc..argCount..ff..', Р±С‹Р»Рѕ '..mc..cfg.Remind.countR..'',mcx)--СЃРѕРѕР±С‰РµРЅРёРµ РѕР± СѓСЃРїРµС€РЅРѕР№ Р·Р°РјРµРЅРµ С‚РµРєСЃС‚Р°
             cfg.Remind.countR = argCount
             inicfg.save(cfg, "Reminder.ini")
         else 
-            sampAddChatMessage(tag..'Введите число ', mcx)
+            sampAddChatMessage(tag..'Р’РІРµРґРёС‚Рµ С‡РёСЃР»Рѕ ', mcx)
         end
     end
 end
 
 function alarm()
     for i = 1, cfg.Remind.countR, 1 do
-         sampAddChatMessage(tag..'Мы напоминаем вам о '..mc..cfg.RemindR.rr, mcx)
+         sampAddChatMessage(tag..'РњС‹ РЅР°РїРѕРјРёРЅР°РµРј РІР°Рј Рѕ '..mc..cfg.RemindR.rr, mcx)
     end
     cfg.RemindOn.remindActivate = false
     inicfg.save(cfg, "Reminder.ini")
